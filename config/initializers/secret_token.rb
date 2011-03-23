25c26d19409678bd91ced0f363b3c6e7454509d9
Brian Gates <briandotgates@gmail.com>
2011-03-23 17:17:33 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cogster::Application.config.secret_token = 'f237d20154f5c7b3d79078e618892fbb33a565f4d2cfaa0530ac6f1903d063b4cc5ab2c71d8a31a079b0165e26cebf14a43b3c6a9dcd2e0700ad050c1751837e'
