a242329172aac1bbf6d3659e33e864717ed49d5e
Brian Gates <briandotgates@gmail.com>
2011-05-02 09:27:22 -0400

    to postgresql

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
+require 'bundler/capistrano'
 set :application,         "cogster"
 set :user,                "deploy"
 set :use_sudo,            false
 set :domain,              "173.230.132.11"
 
+set :scm,                 "git"
 set :repository,          "git@github.com:bgates/cogster.git"
 
 set :git_shallow_clone,   1
@@ -13,6 +15,7 @@ set :deploy_to,           "/srv/www/cogster"
 set :runner,              "deploy"
 
 ssh_options[:paranoid]    = false
+ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
@@ -23,13 +26,15 @@ set :unicorn_binary, "/usr/bin/unicorn"
 set :unicorn_config, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
+
 namespace :deploy do
   task :start, :roles => :app, :except => { :no_release => true } do
     sudo "/etc/init.d/unicorn start"
     #run "cd #{current_path} && #{try_sudo} #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
   task :stop, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} kill `cat #{unicorn_pid}`"
+    sudo "/etc/init.d/unicorn stop"
+    #run "#{try_sudo} kill `cat #{unicorn_pid}`"
   end
   task :graceful_stop, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
@@ -41,4 +46,5 @@ namespace :deploy do
     stop
     start
   end
+
 end
