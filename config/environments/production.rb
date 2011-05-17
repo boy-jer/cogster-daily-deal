0b0bc3aca9af30ed9d72bc1cba5acbf188ff6823
Brian Gates <briandotgates@gmail.com>
2011-05-17 13:30:04 -0400

    please paypal

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Cogster::Application.configure do
   config.active_support.deprecation = :notify
 
   config.after_initialize do
-    ActiveMerchant::Billing::Base.mode = :production
+    ActiveMerchant::Billing::Base.mode = :test
     ::GATEWAY = ActiveMerchant::Billing::PaypalGateway.new(
       #credentials for my sandbox, put on server for testing
        :login => 'brian_1305061324_biz_api1.gmail.com',
@@ -57,6 +57,7 @@ Cogster::Application.configure do
        :signature => 'AhMj-Qk-95Ugtw2EUfTt3VrLYoonA2s75xJwuw.C5dma90wDmlt9S75u'
 
       #below are credentials for steve's real account
+       #when this is set, remember to change mode to production
       #:login => 'chris_api1.cogster.com',#from api key
       #:password => '4929RCZXD247X5FQ', #ditto
       #:signature =>'ARnr1B7zyTW-GnPsQyeq1OlHOUOpAaYyHrRwiti2liIHXowz9rTCR9uQ'
