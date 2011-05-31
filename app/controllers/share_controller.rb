8d3ca0cf001ecd94fcb275f76e6a8ba190ba71cf
Brian Gates <briandotgates@gmail.com>
2011-05-31 12:49:58 -0400

    facebook

--- app/controllers/share_controller.rb
+++ app/controllers/share_controller.rb
@@ -3,14 +3,14 @@ class ShareController < ApplicationController
 
   def facebook
     title = CGI.escape('Cogster: Double Your Money.')
-    url = CGI.escape(business_url(params[:id]))
+    url = CGI.escape("http://www.cogster.com#{business_path(params[:id])}")
     final_url = "http://www.facebook.com/sharer.php?u=#{url}&t=#{title}"
     redirect_to final_url
   end
 
   def twitter
-    destination = business_url(params[:id])
-    url = BitlyResponse.shorten(destination)
+    destination = "http://www.cogster.com#{business_path(params[:id])}"
+    url = BitlyResponse.short_url(destination)
     tweet = "Cogster: Double Your Money. @CogsterDotCom#{url}"
     final_url = "http://twitter.com/home?status=#{CGI.escape(tweet)}"
     redirect_to final_url
