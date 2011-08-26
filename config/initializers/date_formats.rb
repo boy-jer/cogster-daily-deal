38a37c4d222d457bbeb690e4a4f908e26586de0f
Brian Gates <briandotgates@gmail.com>
2011-08-26 05:55:42 -0400

    another attempt to fix date format

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,2 +1,10 @@
 Time::DATE_FORMATS.merge!(:date => "%m/%d/%Y")
 Date::DATE_FORMATS.merge!(:default => "%m/%d/%Y")
+
+require 'date'
+def Date._parse_sla(str, e) # :nodoc:
+  if str.sub!(%r|('?-?\d+)/\s*('?\d+)(?:\D\s*('?-?\d+))?|, ' ') # '
+    s3e(e, $2, $1, $3)
+    true
+  end
+end
